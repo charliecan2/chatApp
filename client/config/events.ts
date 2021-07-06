de91a14b170b1e965455ea1c04236e6eeea4b60b
ccantu20 <carloscq920@gmail.com>
2021-07-05 20:16:15 -0500

    Set up more logic to handle sending messages in the chat rooms

--- client/config/events.ts
+++ client/config/events.ts
@@ -1,8 +1,14 @@
 const EVENTS = {
     connection: 'connection',
     CLIENT: {
-        CREATE_ROOM: 'CREATE_ROOM'
+        CREATE_ROOM: 'CREATE_ROOM',
+        SEND_ROOM_MESSAGE: 'SEND_ROOM_MESSAGE'
+    },
+    SERVER: {
+        ROOMS: 'ROOM',
+        JOINED_ROOM: 'JOINED_ROOM',
+        ROOM_MESSAGE: 'ROOM_MESSAGE'
     }
 }
 
-export default EVENTS;
+export default EVENTS
